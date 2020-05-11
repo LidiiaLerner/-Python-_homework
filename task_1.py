770637fddfac2791479db9063d167983132480cd
Lidiia <65165827+LidiiaLerner@users.noreply.github.com>
2020-05-11 14:02:29 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,11 @@
+
+__author__ = 'Lerner Lidiia'
+
+# Задача-1: Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+a = input("Enter your name: ")
+b = int(input("Enter your age: "))
+c = float(input("Enter any number: "))
+
+print(a, b, c)
